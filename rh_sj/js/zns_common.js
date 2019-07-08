10924c2989b4bb49ae3d2290b1a17daa20f1f70e
黄润斌 <huangrunbin@huangrunbindeMacdiannao.local>
2019-07-08 10:57:41 +0800

    增加判断跳转页

---
+++ rh_sj/js/zns_common.js
@@ -0,0 +1,55 @@
+ $(document).ready(function(){
+ 	
+ 	$('.header_nav').find('ul > li').on('click',function(){
+ 		var biaoti = $(this).html();
+ 		
+ 		if(biaoti == '首页'){
+ 			$(location).attr('href', 'index.html');
+ 		}else if(biaoti == '项目服务'){
+ 			$(location).attr('href', 'xiangmu.html');
+ 		}else if(biaoti == '走进润荭科普'){
+ 			$(location).attr('href', 'runhongkepu.html');
+ 		}else if(biaoti == '服务理念'){
+ 			$(location).attr('href', 'fuwu.html');
+ 		}else if(biaoti == '关于我们'){
+ 			$(location).attr('href', 'guanyu.html');
+ 		}else if(biaoti == '联系我们'){
+ 			$(location).attr('href', 'lianxi.html');
+ 		}
+ 	});
+ 	
+ 	//走进润荭科普页面切换
+ 	$('.gonsi_jiagou').find('li').click(function(){
+ 		$('.gonsi_jiagou').find('li').css('color','#000');
+ 		$(this).css('color','#F34331');
+ 		var zhi = $(this).text();
+ 		if(zhi == '公司介绍'){
+ 			$('.jiesao').css('display','block');
+ 			$('.jiagou').css('display','none');
+ 		}else if(zhi == '公司架构'){
+ 			$('.jiesao').css('display','none');
+ 			$('.jiagou').css('display','block');
+ 		}
+ 	});
+ 	
+ 	//服务项目页面li标签排版
+ 	 var zhi = 0;
+ 	$('.xiangmu').find('li').each(function(){
+        zhi ++;
+        if(zhi%4 == 0){
+        	   $(this).css('float','right').css('margin-right','0');
+        }
+    });
+    
+    //点击页面跳转
+    $('.zonghe').click(function(){
+    	    $(location).attr('href', 'xiangmu.html');
+    });
+    $('.fuwulinian').click(function(){
+      	$(location).attr('href', 'fuwu.html');
+    });
+    $('.lianxifangshi').click(function(){
+    	    $(location).attr('href', 'lianxi.html');
+    });
+ 	
+ });
