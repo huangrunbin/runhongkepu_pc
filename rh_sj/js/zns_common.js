33622824387dbaf0987b40ca09076219445235ed
黄润斌 <huangrunbin@huangrunbindeMacdiannao.local>
2019-07-08 18:59:11 +0800

    daohang

--- rh_sj/js/zns_common.js
+++ rh_sj/js/zns_common.js
@@ -1,7 +1,40 @@
  $(document).ready(function(){
  	
+ 	//导航点击事件
+ 	var dj = 0;
+ 	var clienth_a = document.compatMode === "CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
+ 	$('.zhezhao').css('height',clienth_a+'px');
+	var daohang = $('.home-logo').height();
+	var dibu = $('.footer').height();
+	var zhi = (clienth_a-daohang)-dibu;
+	$('.nav').css('height',zhi+'px');
+ 	$('.header').click(function(){
+	 	if(dj == 0){
+	 		$('.nav').show();
+	 		$('.zhezhao').show();
+	 		$('.nav').css('left','-210px');
+	 		$('.nav').animate({left:'0px'});
+	 		dj = 1;
+	 	}else{
+	 		$('.nav').animate({left:'-210px'});
+	 		setTimeout(function(){
+	 			$('.nav').hide();
+	 		},500);
+	 		dj = 0;
+	 		$('.zhezhao').hide();
+	 	}
+ 	});
  	
- 	
+ 	//点击遮罩隐藏导航
+ 	$('.zhezhao').click(function(){
+ 		$('.nav').animate({left:'-210px'});
+	 	setTimeout(function(){
+	 		$('.nav').hide();
+	 	},500);
+	 	dj = 0;
+	 	$(this).hide();
+ 	});
+   
  	
  	
  	
